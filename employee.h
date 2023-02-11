715d884891c74b9e87bde6e1d1ff72f97c81945b
hua <songyehua_bit@163.com>
2023-02-11 21:17:11 +0800

    4

--- employee.h
+++ employee.h
@@ -7,11 +7,7 @@ int main(){
     cout<<"add somethig"<<endl;
     cout<<"branch conflict"<<endl;
     cout<<"22:50 25%"<<endl;
-<<<<<<< HEAD
-    cout<<"branch dev"<<endl;
-=======
     cout<<"1"<<endl;
->>>>>>> dev
     cout<<"windows modified"<<endl;
     return 0;
 }
